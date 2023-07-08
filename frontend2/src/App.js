7c61fe897de68719e678b73eaaa354ab10485fed
Gayatri <gayatriloungani2021@gmail.com>
2023-07-08 13:02:59 +0530

    first commit

---
+++ frontend2/src/App.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import counter from "./features/counter/Counter";
+import Home from './pages/Home'
+import './App.css';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import {createBrowserRouter , RouterProvider} from "react-router-dom";
+import CartPage from './features/cart/Cart';
+import Checkout from './pages/Checkout';
+import ProductDetailPage from './pages/ProductDetailPage';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      // <Protected>
+        <Home></Home>
+      // </Protected>
+    ),
+  },
+  // {
+  //   path: '/admin',
+  //   element: (
+  //     <ProtectedAdmin>
+  //       <AdminHome></AdminHome>
+  //     </ProtectedAdmin>
+  //   ),
+  // },
+  {
+    path: '/login',
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage></SignupPage>,
+  },
+  {
+    path: '/cart',
+    element: (
+      // <Protected>
+        <CartPage></CartPage>
+      // </Protected>
+   
+    ),
+  },
+  {
+    path: '/checkout',
+    element: (
+      // <Protected>
+        <Checkout></Checkout>
+      // </Protected>
+    ),
+  },
+  {
+    path: '/product-detail',
+    element: (
+      // <Protected>
+        <ProductDetailPage></ProductDetailPage>
+      // </Protected>
+    ),
+  },
+  // {
+  //   path: '/admin/product-detail/:id',
+  //   element: (
+  //     <ProtectedAdmin>
+  //       <AdminProductDetailPage></AdminProductDetailPage>
+  //     </ProtectedAdmin>
+  //   ),
+  // },
+  // {
+  //   path: '/admin/product-form',
+  //   element: (
+  //     <ProtectedAdmin>
+  //       <AdminProductFormPage></AdminProductFormPage>
+  //     </ProtectedAdmin>
+  //   ),
+  // },
+  // {
+  //   path: '/admin/orders',
+  //   element: (
+  //     <ProtectedAdmin>
+  //       <AdminOrdersPage></AdminOrdersPage>
+  //     </ProtectedAdmin>
+  //   ),
+  // },
+  // {
+  //   path: '/admin/product-form/edit/:id',
+  //   element: (
+  //     <ProtectedAdmin>
+  //       <AdminProductFormPage></AdminProductFormPage>
+  //     </ProtectedAdmin>
+  //   ),
+  // },
+  // {
+  //   path: '/order-success/:id',
+  //   element: (
+  //     <Protected>
+  //       <OrderSuccessPage></OrderSuccessPage>{' '}
+  //     </Protected>
+  //   ),
+  // },
+  // {
+  //   path: '/my-orders',
+  //   element: (
+  //     <Protected>
+  //       <UserOrdersPage></UserOrdersPage>{' '}
+  //     </Protected>
+  //   ),
+  // },
+  // {
+  //   path: '/profile',
+  //   element: (
+  //     <Protected>
+  //       <UserProfilePage></UserProfilePage>{' '}
+  //     </Protected>
+  //   ),
+  // },
+  // {
+  //   path: '/stripe-checkout/',
+  //   element: (
+  //     <Protected>
+  //       <StripeCheckout></StripeCheckout>
+  //     </Protected>
+  //   ),
+  // },
+  // {
+  //   path: '/logout',
+  //   element: <Logout></Logout>,
+  // },
+  // {
+  //   path: '/forgot-password',
+  //   element: <ForgotPasswordPage></ForgotPasswordPage>,
+  // },
+  // {
+  //   path: '/reset-password',
+  //   element: <ResetPasswordPage></ResetPasswordPage>,
+  // },
+  // {
+  //   path: '*',
+  //   element: <PageNotFound></PageNotFound>,
+  // },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router = {router} />
+        
+    </div>
+  );
+}
+
+export default App;
